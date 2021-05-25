f8694c566604799bd60029426fca45ab84a96d63
yokota-seia <70831329+yokota-seia@users.noreply.github.com>
2021-05-25 23:13:47 +0900

    配列・オブジェクト
    配列・オブジェクトの課題です

---
+++ JavaScript/1-6/main.js
@@ -0,0 +1,20 @@
+let scores = [10, 15, 20, 25];
+
+console.log(scores);
+
+for (let i = 0; i < scores.length; i++) {
+    if(scores[i] % 2 === 0){
+     console.log(scores[i] + "は偶数です");
+};
+};
+
+let car = {
+    gass: 20.5,
+    num: 1234,
+};
+
+console.log(car);
+
+console.log("ガソリンは" + car.gass + "です");
+
+console.log("ナンバーは" + car.num + "です");
